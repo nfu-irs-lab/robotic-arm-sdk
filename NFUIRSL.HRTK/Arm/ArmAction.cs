1af5e6fbe885996566c01a3f955de802ca1b3db4
ziteh <honmonoh@gmail.com>
2021-03-16 12:30:13 +0800

    #21 Add ArmAction.cs

---
+++ NFUIRSL.HRTK/Arm/ArmAction.cs
@@ -0,0 +1,12 @@
+﻿namespace NFUIRSL.HRTK
+{
+    public interface IArmAction
+    {
+        string Message { get; }
+    }
+
+    public abstract class ArmMotion : IArmAction
+    {
+        public string Message { get; }
+    }
+}
