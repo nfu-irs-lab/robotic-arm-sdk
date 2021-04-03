6707351ce9bba5b7c386f1e7a93fb17d0acfe6e8
ziteh <honmonoh@gmail.com>
2021-04-03 19:43:19 +0800

    #16 增加建構子

--- NFUIRSL.HRTK.Vision/VisionPositioning.cs
+++ NFUIRSL.HRTK.Vision/VisionPositioning.cs
@@ -1,21 +1,47 @@
-﻿using MathNet.Numerics.LinearAlgebra;
+﻿using System;
+using MathNet.Numerics.LinearAlgebra;
 
 namespace NFUIRSL.HRTK.Vision
 {
     public class VisionPositioning
     {
-        private Matrix<double> _armRotationMatrix;
-        private Vector<double> _armTranslationVextor;
-        private Matrix<double> _cameraIntrinsicMatrix;
-        private Matrix<double> _cameraRotationMatrix;
-        private Vector<double> _cameraTranslationVextor;
+        private readonly Matrix<double> _armRotationMatrix;
+        private readonly Vector<double> _armTranslationVector;
+        private readonly Matrix<double> _cameraIntrinsicMatrix;
+        private readonly Matrix<double> _cameraRotationMatrix;
+        private readonly Vector<double> _cameraTranslationVector;
+
+        public VisionPositioning(double[,] cameraIntrinsicMatrix,
+                                 double[] armTranslationVector = null,
+                                 double[,] armRotationMatrix = null,
+                                 double[] cameraTranslationVector = null,
+                                 double[,] cameraRotationMatrix = null)
+        {
+            _cameraIntrinsicMatrix = Matrix<double>.Build.DenseOfArray(cameraIntrinsicMatrix);
+
+            _armTranslationVector = armTranslationVector == null
+                ? Vector<double>.Build.Dense(3, 0)
+                : Vector<double>.Build.DenseOfArray(armTranslationVector);
+
+            _armRotationMatrix = armRotationMatrix == null
+                ? Matrix<double>.Build.DenseOfArray(new double[,] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } })
+                : Matrix<double>.Build.DenseOfArray(armRotationMatrix);
+
+            _cameraTranslationVector = armTranslationVector == null
+                ? Vector<double>.Build.Dense(3, 0)
+                : Vector<double>.Build.DenseOfArray(cameraTranslationVector);
+
+            _cameraRotationMatrix = armRotationMatrix == null
+                ? Matrix<double>.Build.DenseOfArray(new double[,] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } })
+                : Matrix<double>.Build.DenseOfArray(cameraRotationMatrix);
+        }
 
         public double[] ImageToArm(double pixelX, double pixelY)
         {
             var imagePoint = Matrix<double>.Build.DenseOfRowArrays(new[] { pixelX, pixelY, 1 });
 
             var transformation = _armRotationMatrix.SubMatrix(0, 2, 0, 3);
-            transformation = transformation.InsertRow(2, _armTranslationVextor);
+            transformation = transformation.InsertRow(2, _armTranslationVector);
             transformation = transformation.Multiply(_cameraIntrinsicMatrix);
             var result = imagePoint.Multiply(transformation.Inverse());
 
