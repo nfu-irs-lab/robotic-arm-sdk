60bfe33b5e51da364d76a34a8e527ab34be45144
ziteh <honmonoh@gmail.com>
2021-04-03 19:27:33 +0800

    #16 Add basic matrix and vector and ImageToArm()

--- NFUIRSL.HRTK.Vision/VisionPositioning.cs
+++ NFUIRSL.HRTK.Vision/VisionPositioning.cs
@@ -1,5 +1,29 @@
-﻿namespace NFUIRSL.HRTK.Vision
+﻿using MathNet.Numerics.LinearAlgebra;
+
+namespace NFUIRSL.HRTK.Vision
 {
     public class VisionPositioning
-    { }
+    {
+        private Matrix<double> _armRotationMatrix;
+        private Vector<double> _armTranslationVextor;
+        private Matrix<double> _cameraIntrinsicMatrix;
+        private Matrix<double> _cameraRotationMatrix;
+        private Vector<double> _cameraTranslationVextor;
+
+        public double[] ImageToArm(double pixelX, double pixelY)
+        {
+            var imagePoint = Matrix<double>.Build.DenseOfRowArrays(new[] { pixelX, pixelY, 1 });
+
+            var transformation = _armRotationMatrix.SubMatrix(0, 2, 0, 3);
+            transformation = transformation.InsertRow(2, _armTranslationVextor);
+            transformation = transformation.Multiply(_cameraIntrinsicMatrix);
+            var result = imagePoint.Multiply(transformation.Inverse());
+
+            return new[]
+            {
+                (result[0, 0] / result[0, 2]),
+                (result[0, 1] / result[0, 2])
+            };
+        }
+    }
 }
