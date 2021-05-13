9f9c735eea1f008951753fa4b6926e63f16abe5b
ziteh <honmonoh@gmail.com>
2021-05-13 09:33:05 +0800

    Add D.C.F.R.C.C vision position

--- NFUIRSL.HRTK.Vision.UnitTests/VisionPositioningUnitTests.cs
+++ NFUIRSL.HRTK.Vision.UnitTests/VisionPositioningUnitTests.cs
@@ -3,46 +3,46 @@ using NUnit.Framework;
 
 namespace NFUIRSL.HRTK.Vision.UnitTests
 {
-    [TestFixture]
-    public class VisionPositioningUnitTests
-    {
-        // [TestCase(217,198,-120.299, 520.597)]
-        [TestCase(2869, 1863, 102.524, 386.361)]
-        // [TestCase(0,0,0, 0)]
-        public void ImageToArm_InputRealData_OutputRealData(int pixX, int pixY, double armX, double armY)
-        {
-            // Arrange.
-            var vp = MakeVisionPositioning();
-            var exp = new[] { armX, armY };
-            var allowableError = 1;
-
-            // Act.
-            var actual = vp.ImageToArm(pixX, pixY);
-
-            // Assert.
-            var errorX = Math.Abs(actual[0] - exp[0]);
-            var errorY = Math.Abs(actual[1] - exp[1]);
-            Assert.IsTrue(errorX < allowableError);
-            Assert.IsTrue(errorY < allowableError);
-        }
-
-        private VisionPositioning MakeVisionPositioning()
-        {
-            return new VisionPositioning(_intrinsicMatrix, _armTranslationVector);
-        }
-
-        private static readonly double[,] _intrinsicMatrix =
-        {
-            { 5098.50139522781, 0, 0 },
-            { -5.81726849512637, 5097.97821577839, 0 },
-            { 1536.59830718425, 1123.13936955248, 1 }
-        };
-
-        // XXX
-        private static readonly double[] _armTranslationVector =
-            // { -120.299, -520.597 , 0 };
-            // { 120.299, -520.597 , 0 };
-            // { 0, -520.597 ,  -120.299};
-            { 0, -520.597, 120.299 };
-    }
+    // [TestFixture]
+    // public class VisionPositioningUnitTests
+    // {
+    //     // [TestCase(217,198,-120.299, 520.597)]
+    //     [TestCase(2869, 1863, 102.524, 386.361)]
+    //     // [TestCase(0,0,0, 0)]
+    //     public void ImageToArm_InputRealData_OutputRealData(int pixX, int pixY, double armX, double armY)
+    //     {
+    //         // Arrange.
+    //         var vp = MakeVisionPositioning();
+    //         var exp = new[] { armX, armY };
+    //         var allowableError = 1;
+    //
+    //         // Act.
+    //         var actual = vp.ImageToArm(pixX, pixY);
+    //
+    //         // Assert.
+    //         var errorX = Math.Abs(actual[0] - exp[0]);
+    //         var errorY = Math.Abs(actual[1] - exp[1]);
+    //         Assert.IsTrue(errorX < allowableError);
+    //         Assert.IsTrue(errorY < allowableError);
+    //     }
+    //
+    //     private VisionPositioning MakeVisionPositioning()
+    //     {
+    //         return new VisionPositioning(_intrinsicMatrix, _armTranslationVector);
+    //     }
+    //
+    //     private static readonly double[,] _intrinsicMatrix =
+    //     {
+    //         { 5098.50139522781, 0, 0 },
+    //         { -5.81726849512637, 5097.97821577839, 0 },
+    //         { 1536.59830718425, 1123.13936955248, 1 }
+    //     };
+    //
+    //     // XXX
+    //     private static readonly double[] _armTranslationVector =
+    //         // { -120.299, -520.597 , 0 };
+    //         // { 120.299, -520.597 , 0 };
+    //         // { 0, -520.597 ,  -120.299};
+    //         { 0, -520.597, 120.299 };
+    // }
 }
--- NFUIRSL.HRTK.Vision/VisionPositioning.cs
+++ NFUIRSL.HRTK.Vision/VisionPositioning.cs
@@ -1,61 +1,140 @@
 ﻿using System;
 using System.Linq;
 using MathNet.Numerics.LinearAlgebra;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
 
 namespace NFUIRSL.HRTK.Vision
 {
-    public class VisionPositioning
+    public interface IVisionPositioning
     {
-        private readonly Matrix<double> _armRotationMatrix;
-        private readonly Vector<double> _armTranslationVector;
-        private readonly Matrix<double> _cameraIntrinsicMatrix;
-        private readonly Matrix<double> _cameraRotationMatrix;
-        private readonly Vector<double> _cameraTranslationVector;
+        /// <summary>
+        /// Get point of arm by pixel of image.
+        /// </summary>
+        /// <param name="pixelX"></param>
+        /// <param name="pixelY"></param>
+        /// <param name="armX"></param>
+        /// <param name="armY"></param>
+        void ImageToArm(int pixelX, int pixelY, out double armX, out double armY);
+    }
 
-        public VisionPositioning(double[,] cameraIntrinsicMatrix,
-                                 double[] armTranslationVector = null,
-                                 double[,] armRotationMatrix = null,
-                                 double[] cameraTranslationVector = null,
-                                 double[,] cameraRotationMatrix = null)
+    /// <summary>
+    /// Digit-by-digit calculation by Checking Forecast Result with Camera Calibration.<br/>
+    /// 相機標定驗算預測結果逼近算法。
+    /// </summary>
+    public class DCFRCC : IVisionPositioning
+    {
+        private readonly double _allowableError;
+        private readonly CameraParameter _cameraParameter;
+
+        /// <summary>
+        /// Digit-by-digit calculation by Checking Forecast Result with Camera Calibration.<br/>
+        /// 相機標定驗算預測結果逼近算法。
+        /// </summary>
+        public DCFRCC(CameraParameter cameraParameter, double allowableError)
         {
-            // XXX
-            _cameraIntrinsicMatrix = Matrix<double>.Build.DenseOfArray(cameraIntrinsicMatrix);
-            // _cameraIntrinsicMatrix = _cameraIntrinsicMatrix.Transpose();
-
-            _armTranslationVector = armTranslationVector == null
-                ? Vector<double>.Build.Dense(3, 0)
-                : Vector<double>.Build.DenseOfArray(armTranslationVector);
-
-            // XXX
-            _armRotationMatrix = armRotationMatrix == null
-                // ? Matrix<double>.Build.DenseOfArray(new double[,] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } })
-                ? Matrix<double>.Build.DenseOfArray(new double[,] { { 0, 0, 1 }, { 0, 1, 0 }, { 1, 0, 0 } })
-                : Matrix<double>.Build.DenseOfArray(armRotationMatrix);
-
-            _cameraTranslationVector = cameraTranslationVector == null
-                ? Vector<double>.Build.Dense(3, 0)
-                : Vector<double>.Build.DenseOfArray(cameraTranslationVector);
-
-            _cameraRotationMatrix = cameraRotationMatrix == null
-                ? Matrix<double>.Build.DenseOfArray(new double[,] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } })
-                : Matrix<double>.Build.DenseOfArray(cameraRotationMatrix);
+            _cameraParameter = cameraParameter;
+            _allowableError = allowableError;
         }
 
-        public double[] ImageToArm(double pixelX, double pixelY)
+        public void ImageToArm(int pixelX, int pixelY, out double armX, out double armY)
         {
-            // var imagePoint = Matrix<double>.Build.DenseOfRowArrays(new[] { pixelX, pixelY, 1 });
-            var imagePoint = Matrix<double>.Build.DenseOfRowArrays(new[] { 1, pixelY, pixelX });
+            double forecastArmX = 0;
+            double forecastArmY = 0;
 
-            var transformation = _armRotationMatrix.SubMatrix(0, 2, 0, 3);
-            transformation = transformation.InsertRow(2, _armTranslationVector);
-            transformation = transformation.Multiply(_cameraIntrinsicMatrix);
-            var result = imagePoint.Multiply(transformation.Inverse());
-
-            return new[]
+            while (true)
             {
-                (result[0, 0] / result[0, 2]),
-                (result[0, 1] / result[0, 2])
-            };
+                var forecastPixel = CvInvoke.
+                    ProjectPoints(new[] { new MCvPoint3D32f((float)forecastArmX, (float)forecastArmY, 0) },
+                                  new VectorOfDouble(_cameraParameter.RotationVectors),
+                                  new VectorOfDouble(_cameraParameter.TranslationVectors),
+                                  new Emgu.CV.Matrix<double>(_cameraParameter.IntrinsicMatrix),
+                                  new VectorOfDouble(_cameraParameter.DistortionCoefficients));
+
+                double errorX = pixelX - forecastPixel[0].X;
+                double errorY = pixelY - forecastPixel[0].Y;
+
+                if (Math.Abs(errorX) > _allowableError || Math.Abs(errorY) > _allowableError)
+                {
+                    CalOffset(errorX, errorY, ref forecastArmX, ref forecastArmY);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            armX = forecastArmX;
+            armY = forecastArmY;
+        }
+
+        private void CalOffset(double errorX, double errorY, ref double armX, ref double armY)
+        {
+            if (errorX > 0)
+                armX++;
+            else if (errorX < 0)
+                armX--;
+
+            if (errorY > 0)
+                armY++;
+            else if (errorY < 0)
+                armY--;
         }
     }
+
+    // public class VisionPositioning
+    // {
+    //     private readonly Matrix<double> _armRotationMatrix;
+    //     private readonly Vector<double> _armTranslationVector;
+    //     private readonly Matrix<double> _cameraIntrinsicMatrix;
+    //     private readonly Matrix<double> _cameraRotationMatrix;
+    //     private readonly Vector<double> _cameraTranslationVector;
+    //
+    //     public VisionPositioning(double[,] cameraIntrinsicMatrix,
+    //                              double[] armTranslationVector = null,
+    //                              double[,] armRotationMatrix = null,
+    //                              double[] cameraTranslationVector = null,
+    //                              double[,] cameraRotationMatrix = null)
+    //     {
+    //         // XXX
+    //         _cameraIntrinsicMatrix = Matrix<double>.Build.DenseOfArray(cameraIntrinsicMatrix);
+    //         // _cameraIntrinsicMatrix = _cameraIntrinsicMatrix.Transpose();
+    //
+    //         _armTranslationVector = armTranslationVector == null
+    //             ? Vector<double>.Build.Dense(3, 0)
+    //             : Vector<double>.Build.DenseOfArray(armTranslationVector);
+    //
+    //         // XXX
+    //         _armRotationMatrix = armRotationMatrix == null
+    //             // ? Matrix<double>.Build.DenseOfArray(new double[,] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } })
+    //             ? Matrix<double>.Build.DenseOfArray(new double[,] { { 0, 0, 1 }, { 0, 1, 0 }, { 1, 0, 0 } })
+    //             : Matrix<double>.Build.DenseOfArray(armRotationMatrix);
+    //
+    //         _cameraTranslationVector = cameraTranslationVector == null
+    //             ? Vector<double>.Build.Dense(3, 0)
+    //             : Vector<double>.Build.DenseOfArray(cameraTranslationVector);
+    //
+    //         _cameraRotationMatrix = cameraRotationMatrix == null
+    //             ? Matrix<double>.Build.DenseOfArray(new double[,] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } })
+    //             : Matrix<double>.Build.DenseOfArray(cameraRotationMatrix);
+    //     }
+    //
+    //     public double[] ImageToArm(double pixelX, double pixelY)
+    //     {
+    //         // var imagePoint = Matrix<double>.Build.DenseOfRowArrays(new[] { pixelX, pixelY, 1 });
+    //         var imagePoint = Matrix<double>.Build.DenseOfRowArrays(new[] { 1, pixelY, pixelX });
+    //
+    //         var transformation = _armRotationMatrix.SubMatrix(0, 2, 0, 3);
+    //         transformation = transformation.InsertRow(2, _armTranslationVector);
+    //         transformation = transformation.Multiply(_cameraIntrinsicMatrix);
+    //         var result = imagePoint.Multiply(transformation.Inverse());
+    //
+    //         return new[]
+    //         {
+    //             (result[0, 0] / result[0, 2]),
+    //             (result[0, 1] / result[0, 2])
+    //         };
+    //     }
+    // }
 }
