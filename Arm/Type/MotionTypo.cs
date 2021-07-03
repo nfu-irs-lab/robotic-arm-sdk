092b56646344ae1a9b2d7650991af24edf7a612a
ziteh <honmonoh@gmail.com>
2021-07-03 12:42:39 +0800

    Add Arm

---
+++ Arm/Type/MotionTypo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arm.Type
+{
+    /// <summary>
+    /// 運動類型。
+    /// </summary>
+    public enum MotionType
+    {
+        /// <summary>
+        /// 點對點運動。
+        /// </summary>
+        PointToPoint,
+
+        /// <summary>
+        /// 直線運動。
+        /// </summary>
+        Linear,
+
+        /// <summary>
+        /// 圓弧運動。
+        /// </summary>
+        Circle,
+
+        /// <summary>
+        /// 未知的類型。
+        /// </summary>
+        Unknown
+    }
+}
---
+++ Arm/Type/PositionType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arm.Type
+{
+    /// <summary>
+    /// 位置類型。
+    /// </summary>
+    public enum PositionType
+    {
+        /// <summary>
+        /// 絕對位置。
+        /// </summary>
+        Absolute,
+
+        /// <summary>
+        /// 相對位置。
+        /// </summary>
+        Relative,
+
+        /// <summary>
+        /// 未知的位置類型。
+        /// </summary>
+        Unknown
+    }
+}
