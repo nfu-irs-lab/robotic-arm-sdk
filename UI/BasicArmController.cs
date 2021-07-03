1321492ab7abd05d01b486be5d8c7d1e729f1898
ziteh <honmonoh@gmail.com>
2021-07-03 20:22:07 +0800

    Add UserControl BasicArmController.cs

--- UI/BasicArmController.cs
+++ UI/BasicArmController.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace UI
 {
-    public partial class UserControl1: UserControl
+    public partial class BasicArmController : UserControl
     {
-        public UserControl1()
+        public BasicArmController()
         {
             InitializeComponent();
         }
