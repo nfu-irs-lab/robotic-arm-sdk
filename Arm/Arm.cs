1eedc90e023cf42cd7986d303b388974a3f9b4d1
ziteh <honmonoh@gmail.com>
2021-07-07 17:41:23 +0800

    Add Arm.cs

---
+++ Arm/Arm.cs
@@ -0,0 +1,13 @@
+﻿using Basic.Message;
+
+namespace Arm
+{
+    public class Arm
+    {
+        public Arm()
+        {
+            ArmActionFactory armActionFactory = new HiwinArmActionFactory("127.0.0.1", new EmptyMessage());
+            armActionFactory.GetConnect();
+        }
+    }
+}
