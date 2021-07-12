3145b7c8df79ed01a82b551183eec4ccf6dc5b99
ziteh <honmonoh@gmail.com>
2021-07-12 12:54:22 +0800

    Add HiwinJog

--- Arm/Hiwin/HiwinArmActionFactory.cs
+++ Arm/Hiwin/HiwinArmActionFactory.cs
@@ -119,5 +119,10 @@ namespace Arm.Hiwin
         {
             return new HiwinHoming(coordinateType, _id, _message, ref _waiting, needWait);
         }
+
+        public override IJog Jog(string axis, bool needWait = true)
+        {
+            return new HiwinJog(axis, _id, _message, ref _waiting, needWait);
+        }
     }
 }
