7881a7b908043f6170b6055d16df35d67383dd59
ziteh <honmonoh@gmail.com>
2021-08-10 20:40:06 +0800

    #17 Add Connection.cs and Motion.cs

---
+++ RASDK.Arm/TMRobot/Connection.cs
@@ -0,0 +1,17 @@
+﻿namespace RASDK.Arm.TMRobot
+{
+    public class Connection : IConnection
+    {
+        public void Open()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Close()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsOpen { get; }
+    }
+}
