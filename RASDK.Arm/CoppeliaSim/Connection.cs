fe909380b7080f233351f78f81ff8d8bf88e7a44
ziteh <honmonoh@gmail.com>
2021-10-29 15:09:50 +0800

    Created Connected class for CoppeliaSim (#54)

---
+++ RASDK.Arm/CoppeliaSim/Connection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RASDK.Arm.CoppeliaSim
+{
+    internal class Connection : IConnection
+    {
+        public bool IsOpen => throw new NotImplementedException();
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Open()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
