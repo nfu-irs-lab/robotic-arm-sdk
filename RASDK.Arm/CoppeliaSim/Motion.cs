bbec921660a73fa2c3b655a382679b48e0c00cc8
ziteh <honmonoh@gmail.com>
2021-10-29 16:14:30 +0800

    Add UpdteJointHandles() (#54)

--- RASDK.Arm/CoppeliaSim/Motion.cs
+++ RASDK.Arm/CoppeliaSim/Motion.cs
@@ -1,4 +1,5 @@
 ﻿using RASDK.Arm.Type;
+using RASDK.Basic.Message;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,17 @@ using System.Threading.Tasks;
 
 namespace RASDK.Arm.CoppeliaSim
 {
-    internal class Motion : IMotion
+    internal class Motion : BasicAction, IMotion
     {
+        private readonly string _objectName;
+        private int[] _jointHandles = new int[] { -1, -1, -1, -1, -1, -1 };
+
+        public Motion(string objectName, int id, IMessage message) : base(id, message)
+        {
+            _objectName = objectName;
+            UpdateJointHandles();
+        }
+
         public void Abort()
         {
             throw new NotImplementedException();
@@ -42,6 +52,15 @@ namespace RASDK.Arm.CoppeliaSim
         public void Relative(double xJ1, double yJ2, double zJ3, double aJ4, double bJ5, double cJ6, AdditionalMotionParameters addPara = null)
         {
             throw new NotImplementedException();
+        }
+
+        private void UpdateJointHandles()
+        {
+            for (var i = 0; i < _jointHandles.Length; i++)
+            {
+                var jointName = $"{_objectName}_joint{i + 1}";
+                _jointHandles[i] = CoppeliaSimRemoteApi.GetObjectHandle(_id, jointName);
+            }
         }
     }
 }
