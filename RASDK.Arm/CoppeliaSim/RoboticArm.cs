ae22b9faee856392612cc0ccdc493d29c530ef9c
ziteh <honmonoh@gmail.com>
2021-10-29 16:01:29 +0800

    Add ObejctName for CoppeliaSim (#54)

--- RASDK.Arm/CoppeliaSim/RoboticArm.cs
+++ RASDK.Arm/CoppeliaSim/RoboticArm.cs
@@ -12,13 +12,16 @@ namespace RASDK.Arm.CoppeliaSim
     public class RoboticArm : ArmActionFactory, IDevice
     {
         private readonly string _ip;
+        private readonly string _objectName;
         private readonly int _port;
         private int _id = -99;
 
-        public RoboticArm(string ip, int port, IMessage message) : base(message)
+        public RoboticArm(string ip, int port, IMessage message, string objectName = "UR5")
+            : base(message)
         {
             _ip = ip;
             _port = port;
+            _objectName = objectName;
         }
 
         public override double Acceleration { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
@@ -26,6 +29,7 @@ namespace RASDK.Arm.CoppeliaSim
         public int Id => _id;
         public string Ip => _ip;
         public override IMotion Motion => throw new NotImplementedException();
+        public string ObjectName => _objectName;
         public int Port => _port;
         public override double Speed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
