7a93d3c482b632aef7ab251ffe523f0d70dd544f
ziteh <honmonoh@gmail.com>
2021-10-29 16:35:53 +0800

    Add Absolute motion (#54)

--- RASDK.Arm/CoppeliaSim/Motion.cs
+++ RASDK.Arm/CoppeliaSim/Motion.cs
@@ -12,11 +12,23 @@ namespace RASDK.Arm.CoppeliaSim
     {
         private readonly string _objectName;
         private int[] _jointHandles = new int[] { -1, -1, -1, -1, -1, -1 };
+        private CoordinateType CoordinateType = CoordinateType.Descartes;
 
         public Motion(string objectName, int id, IMessage message) : base(id, message)
         {
             _objectName = objectName;
             UpdateJointHandles();
+        }
+
+        private AdditionalMotionParameters _additionalMotionParameters
+        {
+            set
+            {
+                if (value != null)
+                {
+                    CoordinateType = value.CoordinateType;
+                }
+            }
         }
 
         public void Abort()
@@ -26,12 +38,21 @@ namespace RASDK.Arm.CoppeliaSim
 
         public void Absolute(double[] position, AdditionalMotionParameters addPara = null)
         {
-            throw new NotImplementedException();
+            _additionalMotionParameters = addPara;
+            switch (CoordinateType)
+            {
+                case CoordinateType.Joint:
+                    CoppeliaSimRemoteApi.MoveJoint(_id, ConvertToFloatArray(position), _jointHandles, true);
+                    break;
+
+                case CoordinateType.Descartes:
+                    throw new NotImplementedException();
+            }
         }
 
         public void Absolute(double xJ1, double yJ2, double zJ3, double aJ4, double bJ5, double cJ6, AdditionalMotionParameters addPara = null)
         {
-            throw new NotImplementedException();
+            Absolute(new[] { xJ1, yJ2, zJ3, aJ4, bJ5, cJ6 }, addPara);
         }
 
         public void Homing(bool slowly = true, CoordinateType coordinateType = CoordinateType.Descartes, bool needWait = true)
@@ -52,6 +73,16 @@ namespace RASDK.Arm.CoppeliaSim
         public void Relative(double xJ1, double yJ2, double zJ3, double aJ4, double bJ5, double cJ6, AdditionalMotionParameters addPara = null)
         {
             throw new NotImplementedException();
+        }
+
+        private float[] ConvertToFloatArray(double[] value)
+        {
+            float[] fArray = new float[6];
+            for (var i = 0; i < 6; i++)
+            {
+                fArray[i] = (float)value[i];
+            }
+            return fArray;
         }
 
         private void UpdateJointHandles()
