16af25bd982dd3de59827e80989560584dc27d97
ZiTe <honmonoh@gmail.com>
2022-03-16 17:36:21 +0800

    Add doc comments for LogHandler (#75)

--- RASDK.Basic/LogHandler.cs
+++ RASDK.Basic/LogHandler.cs
@@ -39,31 +39,74 @@ namespace RASDK.Basic
         Fatal = byte.MaxValue
     }
 
+    /// <summary>
+    /// 日誌處理器介面。
+    /// </summary>
     public interface ILogHandler
     {
+        /// <summary>
+        /// 日誌檔案路徑。
+        /// </summary>
         string Path { get; }
 
+        /// <summary>
+        /// 寫入日誌。
+        /// </summary>
+        /// <param name="message">訊息。</param>
+        /// <param name="loggingLevel">日誌等級。</param>
         void Write(string message, LoggingLevel loggingLevel);
 
+        /// <summary>
+        /// 寫入日誌。
+        /// </summary>
+        /// <param name="ex">例外情況。</param>
+        /// <param name="loggingLevel">日誌等級。</param>
         void Write(Exception ex, LoggingLevel loggingLevel);
     }
 
+    /// <summary>
+    /// 空的日誌處理器。不實際進行動作。
+    /// </summary>
     public class EmptyLog : ILogHandler
     {
+        /// <summary>
+        /// 日誌檔案路徑。
+        /// </summary>
         public string Path { get; }
 
+        /// <summary>
+        /// 寫入日誌。
+        /// </summary>
+        /// <param name="message">訊息。</param>
+        /// <param name="loggingLevel">日誌等級。</param>
         public void Write(string message, LoggingLevel loggingLevel)
         { }
 
+        /// <summary>
+        /// 寫入日誌。
+        /// </summary>
+        /// <param name="ex">例外情況。</param>
+        /// <param name="loggingLevel">日誌等級。</param>
         public void Write(Exception ex, LoggingLevel loggingLevel)
         { }
     }
 
+    /// <summary>
+    /// 一般的日誌處理器。
+    /// </summary>
     public class LogHandler : ILogHandler
     {
+        /// <summary>
+        /// 要記錄的日誌等級。
+        /// </summary>
         private readonly LoggingLevel LoggingLevel;
         private string Filename;
 
+        /// <summary>
+        /// 一般的日誌處理器。
+        /// </summary>
+        /// <param name="path">日誌檔案路徑。</param>
+        /// <param name="loggingLevel">要記錄的日誌等級。</param>
         public LogHandler(string path = "",
                           LoggingLevel loggingLevel = LoggingLevel.Trace)
         {
@@ -72,18 +115,34 @@ namespace RASDK.Basic
             CreateFile();
         }
 
+        /// <summary>
+        /// 解構子。
+        /// </summary>
         ~LogHandler()
         {
             Write("LogHandler Destruct.", LoggingLevel.Fatal);
         }
 
+        /// <summary>
+        /// 日誌檔案路徑。
+        /// </summary>
         public string Path { get; }
 
+        /// <summary>
+        /// 寫入日誌。
+        /// </summary>
+        /// <param name="ex">例外情況。</param>
+        /// <param name="loggingLevel">日誌等級。</param>
         public void Write(Exception ex, LoggingLevel loggingLevel)
         {
             Write($"{ex.Message}. {ex.StackTrace}", loggingLevel);
         }
 
+        /// <summary>
+        /// 寫入日誌。
+        /// </summary>
+        /// <param name="message">訊息。</param>
+        /// <param name="loggingLevel">日誌等級。</param>
         public void Write(string message, LoggingLevel loggingLevel)
         {
             if (loggingLevel >= LoggingLevel)
@@ -100,19 +159,24 @@ namespace RASDK.Basic
 
         private void CreateFile()
         {
+            // 取得目前的時間。
             var dateTimeNow = DateTime.Now;
             var num = 1;
 
             // Update filename.
             while (true)
             {
+                // 設定目標檔案名稱。
                 var targetFilename = $"{dateTimeNow:MMMdd-HH}_{num}.log";
+
                 if (System.IO.File.Exists(Path + targetFilename))
                 {
+                    // 若目標檔案已經存在，遞增序號，使檔案名稱不重複 。
                     num++;
                 }
                 else
                 {
+                    // 若目標檔案不存在，使用此檔案名稱。
                     Filename = targetFilename;
                     break;
                 }
