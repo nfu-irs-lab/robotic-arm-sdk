f6808afee861a25bbafcbbb9e32e7c4dfad072f8
CST0103 <foolishrockno@gmail.com>
2022-03-23 19:40:06 +0800

    增加Write( )多載(#73)

--- RASDK.Arm/PositionRecorder.cs
+++ RASDK.Arm/PositionRecorder.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RASDK.Basic;
+using RASDK.Arm.Type;
 
 namespace RASDK.Arm
 {
@@ -19,6 +20,13 @@ namespace RASDK.Arm
             CreateFile();
         }
 
+        /// <summary>
+        /// 記錄手臂座標位置。
+        /// </summary>
+        /// <param name="name">位置名稱。</param>
+        /// <param name="position">座標位置。</param>
+        /// <param name="type">座標類型。</param>
+        /// <param name="remark">備註。</param>
         public void Write(string name, double[] position, string type, string remark = "")
         {
             var col = new List<string>();
@@ -39,6 +47,41 @@ namespace RASDK.Arm
             var colName = new List<string>() { "time", "name", "x_j1", "y_j2", "z_j3", "a_j4", "b_j5", "c_j6", "type", "remark" };
             Csv.Write(_path + _filename, row, colName);
         }
+        /// <summary>
+        /// 記錄手臂座標位置。
+        /// </summary>
+        /// <param name="name">位置名稱。</param>
+        /// <param name="position">座標位置。</param>
+        /// <param name="type">座標類型。</param>
+        /// <param name="remark">備註。</param>
+        public void Write(string name, double[] position, CoordinateType type, string remark = "")
+        {
+            Write(name, position, type.ToString(), remark);
+        }
+        /// <summary>
+        /// 記錄手臂位置。
+        /// </summary>
+        /// <param name="name">位置名稱。</param>
+        /// <param name="j1X">座標位置j1/X。</param>
+        /// <param name="j2Y">座標位置j2/Y。</param>
+        /// <param name="j3Z">座標位置j3/Z。</param>
+        /// <param name="j4A">座標位置j4/A。</param>
+        /// <param name="j5B">座標位置j5/B。</param>
+        /// <param name="j6C">座標位置j6/C。</param>
+        /// <param name="type">座標類型。</param>
+        /// <param name="remark">備註。</param>
+        public void Write(string name, double j1X,
+                                        double j2Y,
+                                        double j3Z,
+                                        double j4A,
+                                        double j5B,
+                                        double j6C,
+                                        CoordinateType type,
+                                        string remark = "")
+        {
+            Write(name, new double[] { j1X, j2Y, j3Z, j4A, j5B, j6C }, type, remark);
+        }
+
         private void CreateFile()
         {
             // 取得目前的時間。
