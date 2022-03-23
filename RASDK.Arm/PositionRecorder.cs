4002106ece5cda3bf8fa3ce13341c156fe25135f
CST0103 <foolishrockno@gmail.com>
2022-03-23 19:11:49 +0800

    更改檔案名稱(#73)

--- RASDK.Arm/PositionRecorder.cs
+++ RASDK.Arm/PositionRecorder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,16 @@ namespace RASDK.Arm
 {
     public class PositionRecorder
     {
-        public static void Write(string name, double[] position, string type, string remark = "")
+        private readonly string _path;
+        private string _filename = "";
+
+        public PositionRecorder(string Path = "/")
+        {
+            _path = Path;
+            CreateFile();
+        }
+
+        public void Write(string name, double[] position, string type, string remark = "")
         {
             var col = new List<string>();
 
@@ -27,7 +37,53 @@ namespace RASDK.Arm
             row.Add(col);
 
             var colName = new List<string>() { "time", "name", "x_j1", "y_j2", "z_j3", "a_j4", "b_j5", "c_j6", "type", "remark" };
-            Csv.Write("test.csv", row, colName);
+            Csv.Write(_path + _filename, row, colName);
+        }
+        private void CreateFile()
+        {
+            // 取得目前的時間。
+            var dateTimeNow = DateTime.Now;
+            var num = 1;
+
+            // Update filename.
+            while (true)
+            {
+                // 設定目標檔案名稱。
+                var targetFilename = $"{dateTimeNow:MMMdd-HH}_{num}.csv";
+
+                // 判斷目前檔案是否已經存在。
+                if (System.IO.File.Exists(_path + targetFilename))
+                {
+                    // 若目標檔案已經存在，遞增序號，使檔案名稱不重複 。
+                    num++;
+                }
+                else
+                {
+                    // 若目標檔案不存在，使用此檔案名稱。
+                    _filename = targetFilename;
+                    break;
+                }
+            }
+
+            var sw = MakeStreamWriter();
+            sw.Close();
+        }
+
+        /// <summary>
+        /// Factory pattern。
+        /// </summary>
+        /// <returns></returns>
+        private StreamWriter MakeStreamWriter()
+        {
+            try
+            {
+                return System.IO.File.AppendText(_path + _filename);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Directory.CreateDirectory(_path);
+                return System.IO.File.AppendText(_path + _filename);
+            }
         }
     }
 }
