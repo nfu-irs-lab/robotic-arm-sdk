da46002072b8c164fafb88ddcc8bd22b73b5fd6f
ziteh <honmonoh@gmail.com>
2022-08-15 12:55:24 +0800

    Add a int property for error code

--- RASDK.Arm/Hiwin/Exception.cs
+++ RASDK.Arm/Hiwin/Exception.cs
@@ -21,14 +21,14 @@
         /// HIWIN 機器人控制例外錯誤。
         /// </summary>
         public HiwinRobotControlException(int code) : base($"HIWIN robot error code: {code:0000}.")
-        { }
+        { ErrorCode = code; }
 
         /// <summary>
         /// HIWIN 機器人控制例外錯誤。
         /// </summary>
         public HiwinRobotControlException(int code, string message)
             : base($"HIWIN robot error code: {code:0000}. {message}.")
-        { }
+        { ErrorCode = code; }
 
         /// <summary>
         /// HIWIN 機器人控制例外錯誤。
@@ -36,5 +36,10 @@
         public HiwinRobotControlException(string message, System.Exception innerException)
             : base(message, innerException)
         { }
+
+        /// <summary>
+        /// HIWIN 錯誤代碼。-99999 代表該例外錯誤沒有被指定錯誤代碼。
+        /// </summary>
+        public int ErrorCode { get; protected set; } = -99999;
     }
 }
